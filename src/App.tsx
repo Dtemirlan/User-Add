f38e114ebebc7f9c31a01552adb4379091fa6b1f
Dzhaparov Temirlan <temirlandzaparov820@gmail.com>
2023-11-09 13:20:56 +0600

    Создана папка Components с компонентами UserForm, UserItem, Users

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserForm from './UserForm';
+import Users from './Users';
+
+const App: React.FC = () => {
+    const [users, setUsers] = useState([]);
+
+    const addUser = (user) => {
+        setUsers([...users, user]);
+    };
+
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <UserForm addUser={addUser} />
+                </div>
+                <div className="col-md-6">
+                    <Users users={users} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
